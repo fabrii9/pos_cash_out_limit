af499ab90f68c84358d92c23552673ef76c69a33
Fabrizio Dominguez <78427801+fabrii9@users.noreply.github.com>
2026-09-12 10:51:41 -0300

    Fix Odoo 18 translation metadata for installation

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "POS Cash Out Limit",
     "summary": "Prevent POS cash withdrawals above the available drawer balance",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Point of Sale",
     "license": "LGPL-3",
     "depends": ["point_of_sale"],
